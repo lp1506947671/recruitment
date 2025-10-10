622a1edb42061d521f7064d546e15ed0c65f2fa4
lp1506947671 <xiaopawnye@163.com>
2025-10-11 07:54:45 +0800

    大数据量的关联外键 (autocomplete_fields)

--- src/recruitment/running/admin.py
+++ src/recruitment/running/admin.py
@@ -1,11 +1,39 @@
 from django.contrib import admin
 
-from .models import Area, Cities, Continents, Countries, Regions
+from .models import Cities, Countries, Regions, States
 
-# Register your models here.
 
-admin.site.register(Area)
-admin.site.register(Cities)
-admin.site.register(Continents)
-admin.site.register(Countries)
-admin.site.register(Regions)
+class ReadOnlyAdmin(admin.ModelAdmin):
+    readonly_fields = []
+
+    def get_list_display(self, request):
+        return [field.name for field in self.model._meta.concrete_fields]
+
+
+@admin.register(Countries)
+class CountriesAdmin(ReadOnlyAdmin):
+    search_fields = ("cname",)
+
+
+@admin.register(States)
+class StatesAdmin(ReadOnlyAdmin):
+    search_fields = ("cname",)
+    autocomplete_fields = [
+        "country_id",
+    ]
+
+
+@admin.register(Cities)
+class CitiesAdmin(ReadOnlyAdmin):
+    search_fields = ("cname",)
+    autocomplete_fields = [
+        "state_id",
+    ]
+
+
+@admin.register(Regions)
+class RegionsAdmin(ReadOnlyAdmin):
+    search_fields = ("cname",)
+    autocomplete_fields = [
+        "city_id",
+    ]
--- src/recruitment/running/models.py
+++ src/recruitment/running/models.py
@@ -1,41 +1,6 @@
 from django.db import models
 
 
-class Area(models.Model):
-    country_id = models.IntegerField(blank=True, null=True)
-    code = models.IntegerField(blank=True, null=True)
-    name = models.TextField(blank=True, null=True)
-    cname = models.TextField(blank=True, null=True)
-    lower_name = models.TextField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = "area"
-
-
-class Cities(models.Model):
-    state_id = models.IntegerField()
-    code = models.TextField(blank=True, null=True)
-    name = models.TextField(blank=True, null=True)
-    cname = models.TextField(blank=True, null=True)
-    lower_name = models.TextField(blank=True, null=True)
-    code_full = models.TextField()
-
-    class Meta:
-        managed = False
-        db_table = "cities"
-
-
-class Continents(models.Model):
-    name = models.TextField(blank=True, null=True)
-    cname = models.TextField(blank=True, null=True)
-    lower_name = models.TextField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = "continents"
-
-
 class Countries(models.Model):
     continent_id = models.IntegerField(blank=True, null=True)
     code = models.TextField()
@@ -50,22 +15,14 @@ class Countries(models.Model):
         managed = False
         db_table = "countries"
 
-
-class Regions(models.Model):
-    city_id = models.IntegerField()
-    code = models.TextField(blank=True, null=True)
-    name = models.TextField(blank=True, null=True)
-    cname = models.TextField(blank=True, null=True)
-    lower_name = models.TextField(blank=True, null=True)
-    code_full = models.TextField()
-
-    class Meta:
-        managed = False
-        db_table = "regions"
+    def __str__(self):
+        return self.cname if self.cname else self.full_cname if self.full_cname else ""
 
 
 class States(models.Model):
-    country_id = models.IntegerField()
+    country_id = models.ForeignKey(
+        Countries, db_column="country_id", null=True, on_delete=models.SET_NULL
+    )
     code = models.TextField(blank=True, null=True)
     name = models.TextField(blank=True, null=True)
     cname = models.TextField(blank=True, null=True)
@@ -76,3 +33,42 @@ class States(models.Model):
     class Meta:
         managed = False
         db_table = "states"
+
+    def __str__(self):
+        return self.cname if self.cname else self.name if self.name else ""
+
+
+class Cities(models.Model):
+    state_id = models.ForeignKey(
+        States, db_column="state_id", null=True, on_delete=models.SET_NULL
+    )
+    code = models.TextField(blank=True, null=True)
+    name = models.TextField(blank=True, null=True)
+    cname = models.TextField(blank=True, null=True)
+    lower_name = models.TextField(blank=True, null=True)
+    code_full = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = "cities"
+
+    def __str__(self):
+        return self.cname if self.cname else self.name if self.name else ""
+
+
+class Regions(models.Model):
+    city_id = models.ForeignKey(
+        Cities, db_column="city_id", null=True, on_delete=models.SET_NULL
+    )
+    code = models.TextField(blank=True, null=True)
+    name = models.TextField(blank=True, null=True)
+    cname = models.TextField(blank=True, null=True)
+    lower_name = models.TextField(blank=True, null=True)
+    code_full = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = "regions"
+
+    def __str__(self):
+        return self.cname if self.cname else self.name if self.name else ""
