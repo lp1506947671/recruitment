266c7849c8da88d65dab3871521769bb2035d847
lp1506947671 <xiaopawnye@163.com>
2025-10-11 07:56:11 +0800

    实现只读站点ReadOnlyAdmin

--- src/recruitment/running/admin.py
+++ src/recruitment/running/admin.py
@@ -9,6 +9,22 @@ class ReadOnlyAdmin(admin.ModelAdmin):
     def get_list_display(self, request):
         return [field.name for field in self.model._meta.concrete_fields]
 
+    def get_readonly_fields(self, request, obj=None):
+        return (
+            list(self.readonly_fields)
+            + [field.name for field in obj._meta.fields]
+            + [field.name for field in obj._meta.many_to_many]
+        )
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
 
 @admin.register(Countries)
 class CountriesAdmin(ReadOnlyAdmin):
