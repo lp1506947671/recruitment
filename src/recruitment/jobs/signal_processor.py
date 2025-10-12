9de401ae025f445505f673a2387d22fdd6f4b904
lp1506947671 <xiaopawnye@163.com>
2025-10-12 09:34:28 +0800

    定义信号量

---
+++ src/recruitment/jobs/signal_processor.py
@@ -0,0 +1,46 @@
+import json
+import logging
+
+from django.db.models.signals import post_delete, post_save
+from django.dispatch import receiver
+from django.forms.models import model_to_dict
+from interview.dingtalk import send
+
+from .models import Job, Resume
+
+logger = logging.getLogger(__name__)
+
+
+# 使用 decorator 来注册 信号处理器
+@receiver(signal=post_save, sender=Resume, dispatch_uid="resume_post_save_dispatcher")
+@receiver(signal=post_save, sender=Job, dispatch_uid="job_post_save_dispatcher")
+def post_save_callback(sender, instance=None, created=False, **kwarg):
+    message = ""
+    if isinstance(instance, Job):
+        message = "Job for %s has been saved" % instance.job_name
+    else:
+        message = "Resume for %s %s has been saved " % (
+            instance.username,
+            instance.apply_position,
+        )
+
+    logger.info(message)
+    send(message)
+
+
+def post_delete_callback(sender, instance=None, using=None, **kwarg):
+    dict_obj = model_to_dict(
+        instance, exclude=("picture", "attachment", "created_date", "modified_date")
+    )
+    message = "Instance of %s has been deleted: %s" % (
+        type(instance),
+        json.dumps(dict_obj, ensure_ascii=False),
+    )
+    logger.info(message)
+    send(message)
+
+
+# 手工注册信号处理器
+post_delete.connect(
+    post_delete_callback, sender=Resume, dispatch_uid="resume_post_delete_dispatcher"
+)
